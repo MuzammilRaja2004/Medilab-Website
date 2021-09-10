1efeb56d578a88596e83b4565d59adbcbd38602c
muzammil852 <m.m.muzammilraja@gmail.com>
2021-09-10 08:51:33 -0700

    I Change styleing in website

--- Assest/Js/main.js
+++ Assest/Js/main.js
@@ -13,49 +13,49 @@ const mobilemenu = () =>{
 menu.addEventListener('click',mobilemenu);
 
 // Counter Section Animation 
-var doctor = setInterval(doctor,10)
-var departments = setInterval(departments,25)
-var lab = setInterval(lab,25)
-var awards = setInterval(awards,10)
-let count1 = 1;
-let count2 = 2;
-let count3 = 3;
-let count4 = 4;
+// var doctor = setInterval(doctor,10)
+// var departments = setInterval(departments,25)
+// var lab = setInterval(lab,25)
+// var awards = setInterval(awards,10)
+// let count1 = 1;
+// let count2 = 2;
+// let count3 = 3;
+// let count4 = 4;
 
-function doctor(){
-    count1++;
-    document.querySelector('#dctrNum').innerHTML = count1;
-    // Stop Condition
-    if(count1 == 85)
-    clearInterval(doctor)
-}
+// function doctor(){
+//     count1++;
+//     document.querySelector('#dctrNum').innerHTML = count1;
+//     // Stop Condition
+//     if(count1 == 85)
+//     clearInterval(doctor)
+// }
 
-function departments(){
-    count2++;
-    document.querySelector('#deptNum').innerHTML = count2;
-    // Stop Condition
-    if(count2 == 18)
-    clearInterval(departments)
-}
+// function departments(){
+//     count2++;
+//     document.querySelector('#deptNum').innerHTML = count2;
+//     // Stop Condition
+//     if(count2 == 18)
+//     clearInterval(departments)
+// }
 
-function lab(){
-    count3++;
-    document.querySelector('#labNum').innerHTML = count3;
-    // Stop Condition
-    if(count3 == 12)
-    clearInterval(lab)
-}
+// function lab(){
+//     count3++;
+//     document.querySelector('#labNum').innerHTML = count3;
+//     // Stop Condition
+//     if(count3 == 12)
+//     clearInterval(lab)
+// }
 
-function awards(){
-    count4++;
-    document.querySelector('#awrdNum').innerHTML = count4;
-    // Stop Condition
-    if(count4 == 150)
-    clearInterval(awards)
-    // break
-}
+// function awards(){
+//     count4++;
+//     document.querySelector('#awrdNum').innerHTML = count4;
+//     // Stop Condition
+//     if(count4 == 150)
+//     clearInterval(awards)
+//     break
+// }
+
 // Show Popup
-
 // Popup 1
 var openpopup = document.querySelector('#popup-btn')
 var openpopups = document.querySelector('#popup-btns')
